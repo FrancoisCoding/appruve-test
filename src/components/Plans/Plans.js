effc4c24f2032a7ba5d4b1629a4361803dc7df83
isaiah francois <francoisisaiaht@yahoo.com>
2020-05-03 19:10:27 -0400

    Created functions for API if needed

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Plans.css";
+import axios from "axios";
 
 export const Plans = () => {
+  const [textBody, setTextBody] = useState({
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  });
+
+  useEffect(() => {
+    axios
+      .get(process.env.API_URL)
+      .then((res) => {
+        console.log("Response from API", res);
+        setTextBody(res.data);
+      })
+      .catch((err) => {
+        console.log("API error", err);
+      });
+  }, []);
   return (
     <div className="plans-container">
       <h1 className="header">Choose your plan.</h1>
@@ -9,10 +26,7 @@ export const Plans = () => {
         <div className="card">
           <div className="bronze-bg card-title">BRONZE</div>
           <div className="bronze card-body-top">$5</div>
-          <div className="bronze card-body-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
+          <div className="bronze card-body-text">{textBody.text}</div>
           <div className="arrow-container">
             <i className="fas fa-caret-down bronze"></i>
           </div>
@@ -21,10 +35,7 @@ export const Plans = () => {
         <div className="card">
           <div className="silver-bg card-title">SILVER</div>
           <div className="silver card-body-top">$10</div>
-          <div className="silver card-body-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
+          <div className="silver card-body-text">{textBody.text}</div>
           <div className="arrow-container">
             <i className="fas fa-caret-down silver"></i>
           </div>
@@ -33,10 +44,7 @@ export const Plans = () => {
         <div className="card">
           <div className="gold-bg card-title">GOLD</div>
           <div className="gold card-body-top">$15</div>
-          <div className="gold card-body-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
+          <div className="gold card-body-text">{textBody.text}</div>
           <div className="arrow-container">
             <i className="fas fa-caret-down gold"></i>
           </div>
