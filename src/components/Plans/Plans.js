3d915f11639999354e6e33dd6f1a6f99de543e6b
isaiah francois <francoisisaiaht@yahoo.com>
2020-05-03 18:24:23 -0400

    Start of Test

---
+++ src/components/Plans/Plans.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Plans.css";
+
+export const Plans = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <i className="fas fa-caret-down"></i>
+    </div>
+  );
+};
